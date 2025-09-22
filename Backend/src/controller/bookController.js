22ee3523db417699907a82a7aab0b44784f8c2a6
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-09-22 20:47:32 +0530

    Some minor changes in bookController file

--- Backend/src/controller/bookController.js
+++ Backend/src/controller/bookController.js
@@ -1,5 +1,24 @@
 import Book from "../model/Book";
+import cloudinary from "../lib/cloudinary.js";
 
 const booksController = {
-  create: async (req, res) => {},
+  create: async (req, res) => {
+    try {
+      const { title, caption, rating, image } = req.body;
+      if (!title || !caption || !rating || !image)
+        return res.status(400).json({ message: "All fields must be filled" });
+
+      //upload the image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      const imageUrl = uploadResponse.secure_url;
+      //Save to the data base
+
+      const newBook = new Book({
+        title,
+        caption,
+        rating,
+        image: imageUrl,
+      });
+    } catch (error) {}
+  },
 };
